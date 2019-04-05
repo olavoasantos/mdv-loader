7fb0a2defd29665e07066e5a9dfbfefd77113154
Olavo Amorim Santos <olavo.santos@skipthedishes.ca>
2019-04-05 09:55:25 -0500

    REFACTOR: merged classify with extractImports

--- src/extractImports.js
+++ src/extractImports.js
@@ -1,7 +1,18 @@
-const classifier = require('./classifier');
+const { CODE_BLOCK_REGEX, IMPORT_REGEX } = require('./constants');
 
 module.exports = (raw) => {
-  const { markdown, imports } = classifier(raw);
+  const { markdown, imports } = raw.split('\n').reduce((nodes, line) => {
+    if (IMPORT_REGEX.test(line) && !nodes.isCodeBlock) {
+      nodes.imports.push(line);
+    } else {
+      if (CODE_BLOCK_REGEX.test(line)) {
+        nodes.isCodeBlock = !nodes.isCodeBlock;
+      }
+      nodes.markdown.push(line);
+    }
+
+    return nodes;
+  }, { imports: [], markdown: [], isCodeBlock: false });
 
   return {
     markdown: markdown.join('\n'),
